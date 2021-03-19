af9425329e1041becb056fd11b09776698abd6cb
Thodoris Thomaidis <thodoris.thomaidis@gmail.com>
2021-03-19 15:34:21 +0200

    draft UI - timer works with timer options

---
+++ script.js
@@ -0,0 +1,82 @@
+const mins = document.getElementById('mins')
+const secs = document.getElementById('secs')
+const startBtn = document.getElementById('startBtn')
+const optionsToggle = document.getElementById('optionsToggle')
+const optionsMenu = document.getElementById('optionsMenu')
+
+let optionOne = document.getElementById('option-one')
+let optionTwo = document.getElementById('option-two')
+let optionThree = document.getElementById('option-three')
+let optionFour = document.getElementById('option-four')
+
+let startingTime
+let time
+
+removeDisabled = () => {
+    startBtn.removeAttribute('disabled')
+}
+
+optionOne.onclick = () => {
+    startingTime = 15
+    time = startingTime * 60
+
+    //display
+    mins.innerHTML = `${startingTime}`
+    removeDisabled()
+}
+
+optionTwo.onclick = () => {
+    startingTime = 30
+    time = startingTime * 60
+
+    //display
+    mins.innerHTML = `${startingTime}`
+    removeDisabled()
+}
+
+optionThree.onclick = () => {
+    startingTime = 45
+    time = startingTime * 60
+
+    //display
+    mins.innerHTML = `${startingTime}`
+    removeDisabled()
+}
+
+optionFour.onclick = () => {
+    startingTime = 60
+    time = startingTime * 60
+
+    //display
+    mins.innerHTML = `${startingTime}`
+    removeDisabled()
+}
+
+
+
+function countDown() {
+    if (!startingTime){
+        alert('vale xrono prwta ougkane')  
+    } else {
+        setInterval(() => {
+            let minutes = Math.floor(time/60)
+            let seconds = time % 60
+        
+            minutes = minutes < 10 ? '0' + minutes : minutes
+            seconds = seconds < 10 ? '0' + seconds : seconds
+        
+            mins.innerHTML = `${minutes}`
+            secs.innerHTML = `${seconds}`
+            time--
+        }, 1000)
+    }
+}
+
+startBtn.onclick = () => {
+    countDown()
+    startBtn.setAttribute('disabled', "")
+}
+
+optionsToggle.onclick = () => {
+    optionsMenu.classList.toggle('hide-options')
+}
