5979d20455c6f5c5769acb33790f9c63eab7f24f
Thodoris Thomaidis <thodoris.thomaidis@gmail.com>
2021-03-19 23:35:54 +0200

    fixed endless loop and added reset

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
+//clock
 const mins = document.getElementById('mins')
 const secs = document.getElementById('secs')
+
+//buttons
 const startBtn = document.getElementById('startBtn')
 const optionsToggle = document.getElementById('optionsToggle')
-const optionsMenu = document.getElementById('optionsMenu')
+const resetBtn = document.getElementById('resetBtn')
 
+//timer options
+const optionsMenu = document.getElementById('optionsMenu')
 let optionOne = document.getElementById('option-one')
 let optionTwo = document.getElementById('option-two')
 let optionThree = document.getElementById('option-three')
 let optionFour = document.getElementById('option-four')
-
-let startingTime
-let time
 
 removeDisabled = () => {
     startBtn.removeAttribute('disabled')
@@ -18,68 +20,95 @@ removeDisabled = () => {
 
 //display selected option in the timer
 displaySelectedTimer = () => {
-    mins.innerHTML = `${startingTime}`
+    mins.innerHTML = `${timeMin}`
     removeDisabled()
 }
 
+let timeMin
+let timeSec
+
 optionOne.onclick = () => {
-    startingTime = 0.10
-    time = startingTime * 60
+    timeMin = 0.1
+    timeSec = (timeMin * 60)
 
     displaySelectedTimer()
 }
 
 optionTwo.onclick = () => {
-    startingTime = 30
-    time = startingTime * 60
+    timeMin = 30
+    timeSec = (timeMin * 60)
 
     displaySelectedTimer()
 }
 
 optionThree.onclick = () => {
-    startingTime = 45
-    time = startingTime * 60
+    timeMin = 45
+    timeSec = (timeMin * 60)
 
     displaySelectedTimer()
 }
 
 optionFour.onclick = () => {
-    startingTime = 60
-    time = startingTime * 60
+    timeMin = 60
+    timeSec = (timeMin * 60)
 
     displaySelectedTimer()
 }
 
+let startTimer = null
+
+function stopCountDown() {
+    clearInterval(startTimer)
+    resetDOM()
+}
+
 function countDown() {
     //in case no time setting is selected, display message
-    if (!startingTime){
+    if (!timeMin){
         alert('vale xrono prwta ougkane')  
     } else {
-        setInterval(() => {
-            let minutes = Math.floor(time/60)
-            let seconds = time % 60
-        
+        startTimer = setInterval(() => {
+            let minutes = Math.floor(timeSec/60)
+            let seconds = (timeSec % 60)
+
             //add a 0 in front of single digit numbers
             minutes = minutes < 10 ? '0' + minutes : minutes
             seconds = seconds < 10 ? '0' + seconds : seconds
-        
+            
             //display time left
             mins.innerHTML = `${minutes}`
             secs.innerHTML = `${seconds}`
-            time--
+            
+            timeSec--
 
             //stop the countdown 
-            if (time <= 0) {
-                clearInterval(time = 0)
+            if (timeSec <= 0) {
+                stopCountDown()
             }
-            
+            console.log(timeSec)
+            console.error(seconds)
+
             //DOM changes when timer is started
             optionsMenu.classList.add('hide-options')
             document.body.style.backgroundColor = '#000'
             document.body.style.color = '#F4F4F4'
-            console.log(time)
+            document.title = `${minutes}:${seconds}`
+            
         }, 1000)
     }
+
+    resetBtn.onclick = () => {
+        resetDOM()
+        stopCountDown()
+    }
+}
+
+resetDOM = () => {
+    document.body.style.backgroundColor = '#F4F4F4'
+    document.body.style.color = '#454545'
+    minutes=0
+    seconds=0
+    timeSec=0;
 }
 
 startBtn.onclick = () => {
@@ -90,3 +119,4 @@ startBtn.onclick = () => {
 optionsToggle.onclick = () => {
     optionsMenu.classList.toggle('hide-options')
 }
+
